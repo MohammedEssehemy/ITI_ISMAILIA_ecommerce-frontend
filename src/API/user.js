77c36844596d724751a88a5f74dfd62c36de8077
Mohammed Essehemy <mohammedessehemy@gmail.com>
2019-03-24 19:38:34 +0200

    Fix user api requests

--- src/API/user.js
+++ src/API/user.js
@@ -3,13 +3,13 @@ const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000'
 
 module.exports = {
     register(user) {
-        axios.post(`${baseURL}/users/`, user).then(res => res.data);
+        return axios.post(`${baseURL}/users/`, user).then(res => res.data);
     },
     login(credentials) {
-        axios.post(`${baseURL}/users/authenticate`, credentials).then(res => res.data);
+        return axios.post(`${baseURL}/users/authenticate`, credentials).then(res => res.data);
     },
     getProfile() {
-        axios.get(`${baseURL}/users/profile`, {
+        return axios.get(`${baseURL}/users/profile`, {
             headers: {
                 Authorization: localStorage.getItem('token')
             }
